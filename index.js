ec37b9cc5845d4b1b88536523a652f9448ec4dc8
esfhamilton <esfhamilton@gmail.com>
2020-06-26 15:11:12 +0100

    Setup

--- index.js
+++ index.js
@@ -1,35 +1,25 @@
-/**var app = require('express')();
-var http = require('http').createServer(app);
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
-http.listen(5000, () => {
-  console.log('listening on *:5000');
-});
-**/
-
-const express = require('express');
-const PORT = process.env.PORT || 5000;
-
-express()
-    .set('view engine', 'ejs')
-    .get('/', (req, res) => {res.render('pages/index')})
-    .get('/cool', (req, res) => res.send(cool()))
-    .listen(PORT, () => console.log(`Listening on ${ PORT }`)); 
-
-/**
+const express = require('express')();
 const cool = require('cool-ascii-faces');
-const express = require('express');
-const path = require('path');
 const PORT = process.env.PORT || 5000;
+const http = require('http').createServer(express);
+const io = require('socket.io')(http);
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-  **/
+express.set('view engine', 'ejs');
+express.get('/', (req, res) => {res.render('pages/index')});
+express.get('/cool', (req, res) => res.send(cool()));
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    
+    socket.on('chat message',(msg) => {
+        io.emit('chat message', msg);
+    });
+});
+
+http.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
+});
