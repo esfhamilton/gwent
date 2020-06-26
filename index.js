e8d19fe12701a831986dd482c387226e9fb83cec
esfhamilton <esfhamilton@gmail.com>
2020-06-26 11:57:39 +0100

    Setup

--- index.js
+++ index.js
@@ -3,10 +3,20 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
+var io = require('socket.io')(80);
+var cfg = require('./config.json');
+var tw = require('node-tweet-stream')(cfg);
+tw.track('socket.io');
+tw.track('javascript');
+tw.on('tweet', function(tweet){
+  io.emit('tweet', tweet);
+});
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/room', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
