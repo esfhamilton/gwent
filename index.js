851208cfb6d17d808e5c72dc2a2d164c4df7678b
esfhamilton <esfhamilton@gmail.com>
2020-06-26 12:32:13 +0100

    Setup

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var app = require('express')();
+/**var app = require('express')();
 var http = require('http').createServer(app);
 
 app.get('/', (req, res) => {
@@ -8,3 +8,16 @@ app.get('/', (req, res) => {
 http.listen(5000, () => {
   console.log('listening on *:5000');
 });
+**/
+const cool = require('cool-ascii-faces');
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
