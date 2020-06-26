b8862c026935bcd692e58a6fe21204351160a866
esfhamilton <esfhamilton@gmail.com>
2020-06-26 11:59:25 +0100

    Setup

--- index.js
+++ index.js
@@ -3,15 +3,6 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
-var io = require('socket.io')(80);
-var cfg = require('./config.json');
-var tw = require('node-tweet-stream')(cfg);
-tw.track('socket.io');
-tw.track('javascript');
-tw.on('tweet', function(tweet){
-  io.emit('tweet', tweet);
-});
-
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
