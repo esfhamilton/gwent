182e363faec733ee932b4133cd43b1b4f92d24c5
esfhamilton <esfhamilton@gmail.com>
2020-06-26 12:46:51 +0100

    Setup

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const express = require('express');
 const PORT = process.env.PORT || 5000;
 
 express()
+    .set('view engine', 'ejs')
     .get('/', (req, res) => {res.render('pages/index')})
     .get('/cool', (req, res) => res.send(cool()))
     .listen(PORT, () => console.log(`Listening on ${ PORT }`)); 
