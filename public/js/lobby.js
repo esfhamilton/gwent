4559830c6f1fe99aa09b86d9cee4a29125f9eb56
esfhamilton <esfhamilton@gmail.com>
2020-07-04 22:53:26 +0100

    Added faction selection page

---
+++ public/js/lobby.js
@@ -0,0 +1,39 @@
+const socket = io();
+
+// Get session ID from URL
+const roomQuery = Qs.parse(location.search, {
+    ignoreQueryPrefix: true
+})
+
+let lobbyMsg = document.getElementById("msgPlaceholder");
+
+// IF SID is not present in query => create session
+// ELSE send joinRequest to session SID
+if (Object.keys(roomQuery) != "SID"){
+    socket.emit('createRequest');
+}
+else{
+    const SID = +Object.values(roomQuery);
+    socket.emit('joinRequest', SID);
+}
+
+// Shows session ID to users who have joined a room
+socket.on("validRoom", (SID) => {
+    lobbyMsg.innerHTML = `Session ID: ${SID}`;
+});
+
+// Advances users to faction selection after 2 users have connected to a room
+socket.on("continue", (SID) => {
+    location.replace(`http://localhost:5000/faction.html`);
+});
+
+// Deals with users trying to join a full room
+socket.on("full", (SID) => {    
+    lobbyMsg.innerHTML = `Error: Room ${SID} is full, please create a new room.`;
+});
+
+// Deals with users trying to join a non-existent room
+socket.on("invalidRoom", (SID) => {
+    lobbyMsg.innerHTML = `ERROR: Room ${SID} is an invalid room, please check the session ID has been entered correctly.`;
+});
+
--- server.js
+++ server.js
@@ -65,27 +65,6 @@ io.on('connection', (socket) => {
             }
         });
     });
-    
-    // TEMPORARY - REMOVE AFTER IMPLEMENTING ROOMS
-    socket.on('create or join', function (room) {
-        log('Request to create or join room ' + room);
-
-		var numClients = io.sockets.clients(room).length;
-		log('Room ' + room + ' has ' + numClients + ' client(s)');
-
-		if (numClients === 0){
-			socket.join(room);
-			socket.emit('created', room, socket.id);
-
-		} else if (numClients === 1) {
-			socket.join(room);
-            socket.emit('joined', room, socket.id);
-            io.sockets.in(room).emit('ready');
-
-		} else { // max two clients
-			socket.emit('full', room);
-		}
-	});
 });
 
 
