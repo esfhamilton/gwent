4bd1a56b201934f3e76fd00aa70d902eab94340e
Ethan Hamilton <esfhamilton@gmail.com>
2023-07-19 10:11:12 +0100

    Fix trailing slash and cleanup

--- public/js/faction.js
+++ public/js/faction.js
@@ -10,5 +10,5 @@ socket.emit('rejoinRequest', SID);
 
 function submit() {
     let faction = document.getElementById('faction').value;
-    location.replace(`/deck.html?SID=${SID}&faction=${faction}`);
+    location.replace(`${location.origin}/deck.html?SID=${SID}&faction=${faction}`);
 }
